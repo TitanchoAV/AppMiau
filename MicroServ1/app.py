ce116a3e6114d62890bdb9abbd4ec043b017ce02
TitanchoAV <teban733@gmail.com>
2023-11-16 22:58:54 -0500

    XD

--- MicroServ1/app.py
+++ MicroServ1/app.py
@@ -7,13 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def inicio():
-    return 'Hola mundo desde el microservicio'
-
-@app.route('/enviar',methods=['POST'])
-def datos():
-    a = request.data
-    print(a)
-    return "datos recibidos"
+    return 'MIAU'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=5555)
